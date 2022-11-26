3553cd1aa6afb8b4ac62ea0afa1c55f88e2c311d
Le Minh Nguyen <Lnguyen3388@conestogac.on.ca>
2022-11-26 00:11:58 -0500

    Start Project, Finish Login Form

---
+++ Restaurant/Forgot_Password_Form.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Restaurant
+{
+    public partial class Forgot_Password_Form : Form
+    {
+        public Forgot_Password_Form()
+        {
+            InitializeComponent();
+        }
+        public string username { get;set; }
+        public string password { get;set; }
+    }
+}
