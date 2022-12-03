108fd2557087308a0fa559f2f449936c76698350
gignatov6034 <gignatov6034@conestogac.on.ca>
2022-12-03 18:30:00 -0500

    Minor bugs fixes

--- Restaurant/Program.cs
+++ Restaurant/Program.cs
@@ -16,7 +16,7 @@ namespace Restaurant
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Financial());
+            Application.Run(new Login_Form());
         }
 
     }
